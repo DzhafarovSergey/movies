5cb790b8c44c3abf58ecd60f86b7999d7cf13e2c
Dzhafar <Dzhafarovsa@icloud.com>
2022-01-28 11:29:05 +0300

    CRUD

---
+++ Movie/Movie/Controllers/HelloWordController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Movie.Controllers
+{
+    public class HelloWordController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View(); //визуализирует представление в ответ
+
+        }
+
+        
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ Movie/Movie/Models/CMovies.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Movie.Models
+{
+    public class CMovies
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
